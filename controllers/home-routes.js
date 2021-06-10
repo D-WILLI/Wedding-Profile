34792b1adf08908498f6ac90f79981746ecd1b44
Nish Mitchell <shanishamit@gmail.com>
2021-06-09 20:36:18 -0400

    updating home-routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,37 +1,18 @@
 const router = require('express').Router();
-const { Gallery, Couple } = require('./models');
+const { Gallery, Couple } = require('../models');
 
+// GET one couple
+router.get('/couple/:id', async (req, res) => {
+  try {
+    const CoupleData = await Couple.findByPk(req.params.id);
 
-// Is this a Model, a View, or a Controller? 
-// This file is a Controller. 
-// What is it responsible for handling?
-// It routes commands to the Model and View parts.
+    const couple = CoupleData.get({ plain: true });
 
-
-// Add a comment describing the purpose of the 'get' route
-// GET route for getting all of the dishes that are on the menu
-router.get('/', async (req, res) => {
-  // Add a comment describing the purpose of the render method
-  // This method is rendering the 'all' Handlebars.js template. This is how we connect each route to the correct template.
-  res.render('all');
+    res.render('couple', { couple });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
-
-
-// // GET one couple
-// router.get('/couples/:id', async (req, res) => {
-//   try {
-//     const CoupleData = await Couple.findByPk(req.params.id);
-
-//     const couple = CoupleData.get({ plain: true });
-
-//     res.render('couple', { couple });
-//     console.log('couple')
-//   } catch (err) {
-//     console.log(err);
-//     res.status(500).json(err);
-//   }
-// });
-
-// module.exports = router;
