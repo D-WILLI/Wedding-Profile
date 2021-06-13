4ded008b7e389d6e7d79d617856e73add814385c
Nish Mitchell <shanishamit@gmail.com>
2021-06-13 13:33:40 -0400

    updates

--- db/schema.sql
+++
@@ -1,4 +0,0 @@
-DROP DATABASE IF EXISTS registry_db;
-
-CREATE DATABASE registry_db;
-
--- public/js/script.js
+++
@@ -1,8 +0,0 @@
-let loginBtn = document.querySelector('.loginbtn');
-let loginPage = document.querySelector('.login-page');
-loginBtn.addEventListener('click',function(event){
-  event.preventDefault();
-  loginPage.classList.remove('hide');
-})
-
-$('.asking a').click(function(){$('form').animate({height:'toggle',opacity:'toggle'},'slow');});
